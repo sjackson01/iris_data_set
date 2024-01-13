261e7ceab00abc151c46ceadba3747399c08ab98
Dev\Steven <stevenpjackson01@gmail.com>
2024-01-13 11:35:59 -0600

    feat(iris_data_set): initial formatted

--- ClassifyIrisSpecies.py
+++ ClassifyIrisSpecies.py
@@ -2,4 +2,45 @@ from sklearn.datasets import load_iris
 
 iris_dataset = load_iris()
 
+print("\n")
+
 print("Keysof iris_dataset: \n{}".format(iris_dataset.keys()))
+
+print("\n")
+
+#Parital Description
+print(iris_dataset['DESCR'][:193] + "\n...")
+
+#Full Description 
+#print("Data Set Description: {}".format(iris_dataset['DESCR']))
+
+print("Target names: {}".format(iris_dataset['target_names']))
+
+print("\n")
+
+print("Feature names: {}".format(iris_dataset['feature_names']))
+
+print("\n")
+
+print("Type of data: {}".format(type(iris_dataset['data'])))
+
+print("\n")
+
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+
+print("\n")
+
+print("First five columns of data:\n{}".format(iris_dataset['data'][:5]))
+
+print("\n")
+
+print("Type of target: {}".format(type(iris_dataset['target'])))
+
+print("\n")
+
+print("Shape of target: {}".format(iris_dataset['target'].shape))
+
+print("\n")
+
+print("Target:\n{}".format(iris_dataset['target']))
+
